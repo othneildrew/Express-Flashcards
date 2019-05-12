857783c3887a995460dd3e441b3a0ced5159ff22
Othneil Drew <codeguydrew@gmail.com>
2019-05-11 23:12:43 -0500

    Created basic pug templating

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
-//const pug = require('pug');
 const app = express();
-const port = 3006;
+const port = 3333;
 
-app.set('views', './views')
+
+const colors = ['red', 'blue', 'green', 'yellow', 'red', 'pink'];
+
+
 app.set('view engine', 'pug');
 
 app.listen(port);
 
-
-
-
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/cards', (req, res) => {
+  res.render('card', {
+    prompt: 'This airplane flew 98 seconds when it was first built.',
+    hint: 'Think about the first engine-driven aircraft',
+    colors: colors
+  });
 });
