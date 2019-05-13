7c8a0419263e046befea42381d700b739d46d6a9
Othneil Drew <codeguydrew@gmail.com>
2019-05-12 19:35:03 -0500

    Created error handling, added cards data, separate routes

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3333;
 
-
-const colors = ['red', 'blue', 'green', 'yellow', 'red', 'pink'];
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.listen(port);
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+// Error handling
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.get('/cards', (req, res) => {
-  res.render('card', {
-    prompt: 'This airplane flew 98 seconds when it was first built.',
-    hint: 'Think about the first engine-driven aircraft',
-    colors: colors
-  });
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`The application is running on localhost:${port}`);
 });
