ac3bd8583b55a13c8688cafcd887c562deac8b0a
Othneil Drew <codeguydrew@gmail.com>
2019-05-12 22:57:29 -0500

    Added basic show answer / flip card functionality

--- routes/cards.js
+++ routes/cards.js
@@ -3,11 +3,25 @@ const router = express.Router();
 const { data } = require('../data/flashcards.json');
 const { cards } = data;
 
-router.get('/', (req, res) => {
-  res.render('card', {
-    prompt: cards[0].question,
-    hint: cards[0].hint
-  });
+router.get('/:id', (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+  const { link } = cards[id];
+
+  const templateData = { text };
+
+  if (side === 'question') {
+    templateData.hint = hint;
+    templateData.link = `/cards/${id}?side=answer`;
+  }
+
+  if (side === 'answer') {
+    templateData.link = `/cards/${id}?side=question`;
+  }
+
+  res.render('card', templateData);
 });
 
 
